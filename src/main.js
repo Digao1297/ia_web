62bee3372b2d045da83fbf28d49e6e9060a07001
bed72 <gabriel.jr@aluno.ifsc.edu.br>
2021-02-22 09:30:22 -0300

    :tada: configuração standard js e spectre.css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import app from './app.vue'
 import store from './store'
+import router from './router'
+import './registerServiceWorker'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(app).use(store).use(router).mount('#app')
